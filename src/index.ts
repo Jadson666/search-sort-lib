33671363f784aedfeaad677fc0d702d38c6d346a
Jason Lin <amigo_brian@hotmail.com>
2021-05-16 22:34:48 +0800

    support searching in object

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 const binarySearch = (
   sortedArray: any[],
   target: number,
-  comparator: (ele: any, target: number) => boolean = (ele, target) =>
-    ele === target
+  comparator: (ele: any) => number = (ele) => ele
 ) => {
   let start = 0;
   let end = sortedArray.length - 1;
   while (start <= end) {
     let middle = Math.floor((start + end) / 2);
-    if (comparator(sortedArray[middle], target)) {
+    if (comparator(sortedArray[middle]) === target) {
       return middle;
-    } else if (sortedArray[middle] < target) {
+    } else if (comparator(sortedArray[middle]) < target) {
       start = middle + 1;
     } else {
       end = middle - 1;
--- test/blah.test.ts
+++ test/blah.test.ts
@@ -2,6 +2,11 @@ import { binarySearch } from '../src';
 
 describe('blah', () => {
   it('works', () => {
-    expect(binarySearch([1,2,3,4,5,6], 2)).toEqual(1);
+    expect(binarySearch([1, 2, 3, 4, 5, 6], 2)).toEqual(1);
+  });
+  it('support object', () => {
+    const arr = [{ age: 2 }, { age: 3 }, { age: 6 }];
+    const index = binarySearch(arr, 2, ele => ele.age); // 0
+    expect(index).toEqual(0);
   });
 });
